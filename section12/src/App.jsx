94f06ba669a0866a988343bb22f416903ea8e112
kimseungO <rtd7878@gmail.com>
2026-02-26 20:00:50 +0900

    .

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -10,14 +10,20 @@ import Notfound from './pages/Notfound'
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(), 
+    createdDate: new Date("2026-02-26").getTime(), 
     emotionId: 1,
     content: "1번 일기 내용"
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2026-02-25").getTime(),
     emotionId: 2,
+    content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createdDate: new Date("2026-01-22").getTime(),
+    emotionId: 3,
     content: "2번 일기 내용"
   },
 ]
@@ -36,8 +42,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
@@ -76,16 +82,6 @@ function App() {
 
   return (
     <>
-      <button onClick={() => {
-        onCreate(new Date().getTime(), 1, "Hello");
-      }}>일기 추가 테스트</button>
-      <button onClick={() => {
-        onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다");
-      }}>일기 수정 테스트</button>
-      <button onClick={() => {
-        onDelete(1);
-      }}>일기 삭제 테스트</button>
-
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext.Provider value={{
           onCreate, onDelete, onUpdate
