6e464eaad7a767cfc99a3e2c28381db6510541b1
kimseungO <rtd7878@gmail.com>
2026-02-28 21:01:59 +0900

    .

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { DiaryStateContext } from "../App";
 import Header from "../components/Header"
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
+import usePageTitle from "../hooks/usePageTitle";
+
 
 const getMonthlyData = (pivotDate, data) => {
 
@@ -16,6 +18,7 @@ const getMonthlyData = (pivotDate, data) => {
 const Home = () => {
     const data = useContext(DiaryStateContext);
     const [pivotDate, setPivotDate] = useState(new Date());
+    usePageTitle("감정 일기장")
 
     const monthlyData = getMonthlyData(pivotDate, data);
 
